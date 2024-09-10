ecbf25b6c7ed48370265ccde414774ca21fc2c00
jaelynnkim <jaevkim@gmail.com>
2024-09-10 00:42:05 -0400

    Update request_form.py
    Forgot to change "SF_DOMAIN" to "test" instead of "login"

--- request_form.py
+++ request_form.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Request Form", page_icon="📝")
 SF_USERNAME = os.getenv("SF_USERNAME")  # No default value; ensure environment variable is set
 SF_PASSWORD = os.getenv("SF_PASSWORD")
 SF_SECURITY_TOKEN = os.getenv("SF_SECURITY_TOKEN")
-SF_DOMAIN = 'login'  # Use 'test' if connecting to a Salesforce sandbox
+SF_DOMAIN = 'test'  # Use 'test' if connecting to a Salesforce sandbox
 
 # Connect to Salesforce
 try:
